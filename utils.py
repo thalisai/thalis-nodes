7b94d2f283b70e4291fda1c4e32e7d2491db25c3
thalismind <admin@thalis.dev>
2025-01-11 21:03:29 -0600

    implement actual download

---
+++ utils.py
@@ -0,0 +1,24 @@
+import requests
+from tqdm import tqdm
+
+def download_url(url: str, filepath: str) -> None:
+    """
+    Stream the given URL to the given file path.
+    """
+
+    # Streaming, so we can iterate over the response.
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+
+    # Sizes in bytes.
+    total_size = int(response.headers.get("content-length", 0))
+    block_size = 1024 * 1024 * 4  # 4 MB
+
+    with tqdm(total=total_size, unit="B", unit_scale=True) as progress_bar:
+        with open(filepath, "wb") as file:
+            for data in response.iter_content(block_size):
+                progress_bar.update(len(data))
+                file.write(data)
+
+    if total_size != 0 and progress_bar.n != total_size:
+        raise RuntimeError("Could not download file")
--- web.py
+++ web.py
@@ -4,6 +4,8 @@ from aiohttp import web
 from folder_paths import folder_names_and_paths, models_dir
 from server import PromptServer
 
+from .utils import download_url
+
 folder_names_and_paths["workflows"] = ([os.path.join(models_dir, "workflows")], [".json", ".yaml"])
 
 routes = web.RouteTableDef()
@@ -23,7 +25,10 @@ async def download_model(request):
     if model_name is None:
         return web.Response(status=400, text="model_name not found in request")
 
-    print("Downloading model", request, folder, source_url, model_name)
+    dest_folder = folder_names_and_paths[folder][0][0]
+    print("Downloading model", request, folder, source_url, model_name, dest_folder)
+    download_url(source_url, os.path.join(dest_folder, model_name))
+    return web.Response(status=200, text="Model downloaded")
 
-print("Adding routes")
+print("Adding Thalis AI routes")
 PromptServer.instance.app.add_routes(routes)
